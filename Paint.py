82292c1695a406f50b8deb45a61f273eb0af667d
PLI Harvey <pli.harvey@gmail.com>
2013-07-07 15:47:07 -0400

    Assignment 3 - Under Development
    Paint.py just finished getting all the colour aspects on this to work.

--- Paint.py
+++ Paint.py
@@ -4,13 +4,9 @@
 #Last Modified On: 2012-07-13
 #Program Description: Paint Program
 #Revision History:
-#  - Using one big If as the top of the tree
-#  - Has working buttons, and everything
+#  - Colour choice for outline and fill are working properly
 
 import pygame, Buttons
-
-def chooseColour():
-    
 
 def text(toPrint):
     font = pygame.font.SysFont("None", 30)
@@ -26,7 +22,7 @@ def main():
 
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill((255,0,0))
+    background.fill((150,150,150))
 
     ##    #buttons
     lines = Buttons.Button()
@@ -34,8 +30,27 @@ def main():
     clear = Buttons.Button()
     save = Buttons.Button()
     load = Buttons.Button()
+
+    #colour Buttons
+    colourArea = Buttons.Button()
+    colourPicker = Buttons.Button()
+    colourViewer = pygame.Surface((100,100))
+    colourViewer = colourViewer.convert()
     colour1 = Buttons.Button()
     colour2 = Buttons.Button()
+    redBtn = Buttons.Button()
+    greenBtn = Buttons.Button()
+    blueBtn = Buttons.Button()
+    #Preset colour buttons
+    clRed = Buttons.Button()
+    clGrn = Buttons.Button()
+    clBlu = Buttons.Button()
+    clOrg = Buttons.Button()
+    clYel = Buttons.Button()
+    clPur = Buttons.Button()
+    clBlk = Buttons.Button()
+    clWht = Buttons.Button()
+    
 
     canvas = pygame.Surface((500,500))
     canvas = canvas.convert()
@@ -47,7 +62,7 @@ def main():
 
     bk = pygame.Surface(tools.get_size())
     bk = bk.convert()
-    bk.fill((0,0,255))
+    bk.fill((150,150,150))
 
     #Buttons for Lines
     str8Line = Buttons.Button()
@@ -65,22 +80,32 @@ def main():
 ##        Initial Variables
 
     clock = pygame.time.Clock()
+    colourChoice = False
     preview = False
     toolMenu = True
     keepGoing = True
     LineStart = (0,0)
     lineEnd = (0,0)
 
+    selectedLocation = (550,550)
+    selectedValue = ""
+    println = ""
     activeEvent = "line"
     selectedLine = "str8line.png"
     selectedShape = "square.png"
     colourOutline = (0,0,0)
     colourFill = (255,255,255)
+    redVal = 0
+    greenVal = 0
+    blueVal = 0
+    chosenColour = (redVal,greenVal,blueVal)
     
     while (keepGoing):
         clock.tick(30)
 
-        errorMess = text(str(lineEnd))
+##        errorMess = text(str(lineEnd))
+        userTips = text(println)
+        rgbValue = text(str(chosenColour))
 
         if toolMenu:
             for event in pygame.event.get():
@@ -99,31 +124,155 @@ def main():
                         else:
                             toolMenu = True
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if str8Line.onClick(pygame.mouse.get_pos()):
-                        selectedLine = "str8line.png"
-                    elif curvedLine.onClick(pygame.mouse.get_pos()):
-                        selectedLine = "curvedline.png"
-                    elif sqrOutline.onClick(pygame.mouse.get_pos()):
-                        selectedShape = "sqaure.png"
-                    elif crOutline.onClick(pygame.mouse.get_pos()):
-                        selectedShape = "circle.png"
-                    elif sqrFill.onClick(pygame.mouse.get_pos()):
-                        selectedShape = "squareFill.png"
-                    elif crFill.onClick(pygame.mouse.get_pos()):
-                        selectedShape = "circleFill.png"
-                    elif colour1.onClick(pygme.mouse.get_pos()):
-                        colourOutline = chooseColour()
-                    elif colour2.onClick(pygame.mouse.get_pos()):
-                        colourFill = chooseColour()
+                    if colourChoice == False:
+                        if str8Line.onClick(pygame.mouse.get_pos()):
+                            selectedLine = "str8line.png"
+                        elif curvedLine.onClick(pygame.mouse.get_pos()):
+                            selectedLine = "curvedline.png"
+                        elif sqrOutline.onClick(pygame.mouse.get_pos()):
+                            selectedShape = "sqaure.png"
+                        elif crOutline.onClick(pygame.mouse.get_pos()):
+                            selectedShape = "circle.png"
+                        elif sqrFill.onClick(pygame.mouse.get_pos()):
+                            selectedShape = "squareFill.png"
+                        elif crFill.onClick(pygame.mouse.get_pos()):
+                            selectedShape = "circleFill.png"
+                        elif colourArea.onClick(pygame.mouse.get_pos()):
+                            println = "Please select a colour to edit"
+                        elif colour1.onClick(pygame.mouse.get_pos()):
+                            colourChoice = True
+                            selectedColour = "outline"
+                            chosenColour = colourOutline
+                            println = str(colourOutline)
+                        elif colour2.onClick(pygame.mouse.get_pos()):
+                            colourChoice = True
+                            selectedColour = "fill"
+                            chosenColour = colourFill
+                            println = str(colourFill)
+
+                if colourChoice:
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if colourArea.onClick(pygame.mouse.get_pos()):
+                            if colourPicker.onClick(pygame.mouse.get_pos()):
+                                toolMenu = False
+                                activeEvent = "Colour Picker"
+                                
+                            if selectedColour == "outline":
+                                println = "Editing the outline colour"
+                            elif selectedColour == "fill":
+                                println = "Editing the fill colour"
+                                
+                            #For pre-set colours
+                            if clRed.onClick(pygame.mouse.get_pos()):
+                                chosenColour = (255,0,0)
+                            elif clGrn.onClick(pygame.mouse.get_pos()):
+                                chosenColour = (0,255,0)
+                            elif clBlu.onClick(pygame.mouse.get_pos()):
+                                chosenColour = (0,0,255)
+                            elif clOrg.onClick(pygame.mouse.get_pos()):
+                                chosenColour = (255,165,0)
+                            elif clYel.onClick(pygame.mouse.get_pos()):
+                                chosenColour = (255,255,0)
+                            elif clPur.onClick(pygame.mouse.get_pos()):
+                                chosenColour = (128,0,128)
+                            elif clBlk.onClick(pygame.mouse.get_pos()):
+                                chosenColour = (0,0,0)
+                            elif clWht.onClick(pygame.mouse.get_pos()):
+                                chosenColour = (255,255,255)
+
+                            redVal = chosenColour[0]
+                            greenVal = chosenColour[1]
+                            blueVal = chosenColour[2]
+                                
+                            if redBtn.onClick(pygame.mouse.get_pos()):
+                                selectedValue = "red"
+                                value = redVal
+                            elif greenBtn.onClick(pygame.mouse.get_pos()):
+                                selectedValue = "green"
+                                value = greenVal
+                            elif blueBtn.onClick(pygame.mouse.get_pos()):
+                                selectedValue = "blue"
+                                value = blueVal
+                            else:
+                                selectedValue = ""
+
+                            
+                                
+                        else:
+                            println = "Press 'Enter' to set Colour Choice"
+
+                    
+                    if event.type == pygame.KEYDOWN:
+                        if selectedValue != "":
+                            if event.key == pygame.K_UP:
+                                if selectedValue == "red":
+                                    redVal += 5
+                                elif selectedValue == "green":
+                                    greenVal += 5
+                                elif selectedValue == "blue":
+                                    blueVal += 5
+                            elif event.key == pygame.K_DOWN:
+                                if selectedValue == "red":
+                                    redVal -= 5
+                                elif selectedValue == "green":
+                                    greenVal -= 5
+                                elif selectedValue == "blue":
+                                    blueVal -= 5
+
+                            if redVal > 255:
+                                redVal = 255
+                            elif redVal < 0:
+                                redVal = 0
+                            elif greenVal > 255:
+                                greenVal = 255
+                            elif greenVal < 0:
+                                greenVal = 0
+                            elif blueVal > 255:
+                                blueVal = 255
+                            elif blueVal < 0:
+                                blueVal = 0
+
+                            chosenColour = (redVal,greenVal,blueVal)
+                                
+                        if event.key == pygame.K_RETURN:
+                            colourChoice = False
+                            if selectedColour == "outline":
+                                colourOutline = chosenColour
+                            elif selectedColour == "fill":
+                                colourFill = chosenColour
+
+                        
                         
             tools.blit(bk, (0,0))
-            screen.blit(errorMess, (0,510))
+            screen.blit(userTips, (0,510))
+
+##            screen.blit(selected, selectedLocation)
+            
             str8Line.graphic(bk,30,30,420,125,"str8line.png")
             curvedLine.graphic(bk,30,30,460,125,"curvedline.png")
             sqrOutline.graphic(bk,30,30,340,165,"square.png")
             crOutline.graphic(bk,30,30,380,165,"circle.png")
             sqrFill.graphic(bk,30,30,420,165,"squareFill.png")
             crFill.graphic(bk,30,30,460,165,"circleFill.png")
+
+            #Everything for colour choice area
+            screen.blit(rgbValue, (100,275))
+            colourArea.invisiSquare(bk,295,155,90,290," ",(0,0,0))
+            colourViewer.fill(chosenColour)
+            screen.blit(colourViewer, (100,300))
+            redBtn.labeledButton(bk,(255,0,0),50,25,250,300," R",(0,0,0))
+            greenBtn.labeledButton(bk,(0,255,0),50,24,250,335," G",(0,0,0))
+            blueBtn.labeledButton(bk,(0,0,255),50,25,250,370," B",(0,0,0))
+            colourPicker.graphic(bk,30,30,325,310,"dropper.png")
+            #Including preset colours
+            clRed.square(bk,(255,0,0),30,30,100,405)
+            clGrn.square(bk,(0,255,0),30,30,135,405)
+            clBlu.square(bk,(0,0,255),30,30,170,405)
+            clOrg.square(bk,(255,165,0),30,30,205,405)
+            clYel.square(bk,(255,255,0),30,30,240,405)
+            clPur.square(bk,(128,0,128),30,30,275,405)
+            clBlk.square(bk,(0,0,0),30,30,310,405)
+            clWht.square(bk,(255,255,255),30,30,345,405)
 
             #Shows tool bar on the Canvas screen
             lines.graphic(bk, 30,30,510,125,selectedLine)
@@ -147,7 +296,12 @@ def main():
                             toolMenu = False
                         else:
                             toolMenu = True
+                    elif event.key == pygame.K_RETURN:
+                        activeEvent = ""
                 elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if pygame.mouse.get_pos() > (500,500):
+                        activeEvent = ""
+                        println = ""
                     if clear.onClick(pygame.mouse.get_pos()):
                         canvas.fill((255,255,255))
                     elif save.onClick(pygame.mouse.get_pos()):
@@ -158,16 +312,20 @@ def main():
                         activeEvent = "line"
                     elif shapes.onClick(pygame.mouse.get_pos()):
                         activeEvent = "shape"
+                    elif colourPicker.onClick(pygame.mouse.get_pos()):
+                        activeEvent = "colour Picker"
                     elif colour1.onClick(pygame.mouse.get_pos()):
                         toolMenu = True
-                        activeEvent = ""
+                        colourChoice = True
+                        selectedColour = "outline"
                     elif colour2.onClick(pygame.mouse.get_pos()):
                         toolMenu = True
-                        activeEvent = ""
+                        colourChoice = True
+                        selectedColour = "fill"
 
-                        
                 if activeEvent == "line":
                     if selectedLine == "str8line.png":
+                        println = "Draw a straight line"
                         if event.type == pygame.MOUSEBUTTONDOWN:
                             lineStart = pygame.mouse.get_pos()
                             preview = True
@@ -178,6 +336,7 @@ def main():
                         elif event.type == pygame.MOUSEMOTION:
                             lineEnd = pygame.mouse.get_pos()
                     elif selectedLine == "curvedline.png":
+                        println = "Draw a curved line"
                         if event.type == pygame.MOUSEMOTION:
                             lineEnd = pygame.mouse.get_pos()
                             if pygame.mouse.get_pressed() == (1,0,0):
@@ -186,6 +345,7 @@ def main():
                                 
                 elif activeEvent == "shape":
                     if selectedShape == "square.png":
+                        println = "Draw an outline of a rectangle"
                         if event.type == pygame.MOUSEBUTTONDOWN:
                             lineStart = pygame.mouse.get_pos()
                             lineEnd = (0,0)
@@ -199,6 +359,7 @@ def main():
                             currentPos = pygame.mouse.get_pos()
                             lineEnd = (currentPos[0]-lineStart[0], currentPos[1]-lineStart[1])
                     elif selectedShape == "circle.png":
+                        println = "Draw an outline of a circle"
                         if event.type == pygame.MOUSEBUTTONDOWN:
                             lineStart = pygame.mouse.get_pos()
                             lineEnd = (0,0)
@@ -213,6 +374,7 @@ def main():
                             currentPos = pygame.mouse.get_pos()
                             lineEnd = (currentPos[0]-lineStart[0], currentPos[1]-lineStart[1])
                     elif selectedShape == "squareFill.png":
+                        println = "Draw a rectangle filled with colour"
                         if event.type == pygame.MOUSEBUTTONDOWN:
                             lineStart = pygame.mouse.get_pos()
                             lineEnd = (0,0)
@@ -227,6 +389,7 @@ def main():
                             currentPos = pygame.mouse.get_pos()
                             lineEnd = (currentPos[0]-lineStart[0], currentPos[1]-lineStart[1])
                     elif selectedShape == "circleFill.png":
+                        println = "Draw a circle filled with colour"
                         if event.type == pygame.MOUSEBUTTONDOWN:
                             lineStart = pygame.mouse.get_pos()
                             lineEnd = (0,0)
@@ -242,11 +405,21 @@ def main():
                             currentPos = pygame.mouse.get_pos()
                             lineEnd = (currentPos[0]-lineStart[0], currentPos[1]-lineStart[1])
                     
+                elif activeEvent == "Colour Picker":
+                    println = "Select a colour to work with"
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        chosenColour = tuple(canvas.get_at(pygame.mouse.get_pos()))
+                        redVal = chosenColour[0]
+                        greenVal = chosenColour[1]
+                        blueVal = chosenColour[2]
+                        println = "("+str(redVal)+","+str(greenVal)+","+str(blueVal)+")"
+                        toolMenu = True                   
+                    
 
                             
             screen.blit(background, (0, 0))
             screen.blit(canvas, (0,0))
-            screen.blit(errorMess, (0,510))
+            screen.blit(userTips, (0,510))
             clear.graphic(background, 30,30,510,5,"clear.png")
             save.graphic(background, 30,30,510,40,"save.png")
             load.graphic(background, 30,30,510,75,"load.png")
